61250bf992a36520db80661b901e83b9bda03495
withlet11 <withlet11@gmail.com>
2025-01-09 21:34:07 +0100

    Migrated build.gradle files from Groovy to Kotlin.

---
+++ app/build.gradle.kts
@@ -0,0 +1,78 @@
+import java.util.Properties
+import java.io.FileInputStream
+
+plugins {
+    id("com.android.application")
+    id("com.google.android.gms.oss-licenses-plugin")
+    id("kotlin-android")
+    // id("kotlin-android-extensions")
+}
+
+// Create a variable called keystorePropertiesFile, and initialize it to your
+// keystore.properties file, in the rootProject folder.
+val keystorePropertiesFile = rootProject.file("keystore.properties")
+
+// Initialize a new Properties() object called keystoreProperties.
+val keystoreProperties = Properties()
+
+// Load your keystore.properties file into the keystoreProperties object.
+keystoreProperties.load(FileInputStream(keystorePropertiesFile))
+
+android {
+    signingConfigs {
+        create("config") {
+            keyAlias = keystoreProperties["keyAlias"] as String
+            keyPassword = keystoreProperties["keyPassword"] as String
+            storeFile = file(keystoreProperties["storeFile"] as String)
+            storePassword = keystoreProperties["storePassword"] as String
+        }
+    }
+
+    compileSdk = 35
+
+    defaultConfig {
+        applicationId = "io.github.withlet11.digitalquadrant"
+        minSdk = 26
+        targetSdk = 35
+        versionCode = 4
+        versionName = "1.3"
+
+        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+    }
+
+    buildTypes {
+        getByName("release") {
+            isMinifyEnabled = false
+            proguardFiles(getDefaultProguardFile("proguard-android-optimize.txt"), "proguard-rules.pro")
+            signingConfig = signingConfigs.getByName("config")
+        }
+    }
+    namespace = "io.github.withlet11.digitalquadrant"
+
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
+    }
+}
+
+kotlin {
+    jvmToolchain(17)
+}
+
+dependencies {
+    val kotlin_version: String by project
+
+    implementation(fileTree(mapOf("dir" to "libs", "include" to listOf("*.jar"))))
+    implementation("org.jetbrains.kotlin:kotlin-stdlib:$kotlin_version")
+    implementation("androidx.core:core-ktx:1.15.0")
+    implementation("androidx.appcompat:appcompat:1.7.0")
+    implementation("androidx.constraintlayout:constraintlayout:2.2.0")
+    implementation("androidx.viewpager2:viewpager2:1.1.0")
+    implementation("com.google.android.gms:play-services-oss-licenses:17.1.0")
+    implementation("androidx.camera:camera-lifecycle:1.4.1")
+    implementation("androidx.lifecycle:lifecycle-common-jvm:2.8.7")
+    implementation("androidx.camera:camera-view:1.4.1")
+    testImplementation("junit:junit:4.13.2")
+    androidTestImplementation("androidx.test.ext:junit:1.2.1")
+    androidTestImplementation("androidx.test.espresso:espresso-core:3.6.1")
+}
--- build.gradle.kts
+++ build.gradle.kts
@@ -1,17 +1,18 @@
 // Top-level build file where you can add configuration options common to all sub-projects/modules.
 buildscript {
-    ext.kotlin_version = '2.0.21'
+    val kotlin_version by extra("2.0.21")
     repositories {
         google()
         mavenCentral()
     }
+
     dependencies {
-        classpath 'com.android.tools.build:gradle:8.7.3'
-        classpath "org.jetbrains.kotlin:kotlin-gradle-plugin:$kotlin_version"
+        classpath("com.android.tools.build:gradle:8.7.3")
+        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:$kotlin_version")
 
         // NOTE: Do not place your application dependencies here; they belong
         // in the individual module build.gradle files
-        classpath 'com.google.android.gms:oss-licenses-plugin:0.10.6'
+        classpath("com.google.android.gms:oss-licenses-plugin:0.10.6")
     }
 }
 
@@ -22,6 +23,6 @@ allprojects {
     }
 }
 
-tasks.register("clean", Delete) {
-    delete rootProject.buildDirectory
+tasks.register("clean", Delete::class) {
+    delete(rootProject.layout.buildDirectory)
 }
