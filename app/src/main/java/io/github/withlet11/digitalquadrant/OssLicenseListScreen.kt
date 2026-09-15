5f187dd9a850f79cd5a5cf7c2498397c82cc21cb
withlet11 <withlet11@gmail.com>
2026-09-15 19:29:12 +0200

    Removed Compose BOM and unused import in OssLicenseListScreen.
    - Removed `androidx-compose-bom` dependency from `libs.versions.toml`.
    - Removed unused `padding` import from `OssLicenseListScreen.kt`.

--- app/src/main/java/io/github/withlet11/digitalquadrant/OssLicenseListScreen.kt
+++ app/src/main/java/io/github/withlet11/digitalquadrant/OssLicenseListScreen.kt
@@ -28,7 +28,6 @@ import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.calculateEndPadding
 import androidx.compose.foundation.layout.calculateStartPadding
 import androidx.compose.foundation.layout.height
-import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.material3.HorizontalDivider
